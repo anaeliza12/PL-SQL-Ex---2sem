7833608aeeec136a1825541b7919dae3ac82a518
Ana Eliza <anaelizaperobelli@gmail.com>
2024-08-13 20:13:11 -0300

    cursor

--- ExCursores1.sql
+++ ExCursores1.sql
@@ -2,5 +2,26 @@ CREATE TABLE top_dogs
 (name VARCHAR(25),
 salary NUMBER(11,2))
 
+SET VERIFY OFF
+ACCEPT p_num prompt 'Informe o numero de maior salario'
+DECLARE
 
+ cursor cur_Dept is
+       select last_name,salary
+       from employees
+       where rownum <= &p_num
+       order by salary desc;
+reg_top_sal cur_Dept%rowtype;
+
+BEGIN
+ open   cur_Dept;
+  fetch cur_Dept into reg_top_sal;
+  while cur_Dept%found loop
+     dbms_output.put_line (reg_top_sal.last_name|| ' ' || reg_top_sal.salary);
+     INSERT INTO top_dogs VALUES (reg_top_sal.last_name,reg_top_sal.salary);
+     fetch cur_Dept into reg_top_sal;
+     end loop;
+     commit;
+  close cur_Dept;
+end;
 
